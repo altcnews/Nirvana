9e6bda962802e146b46c83b568497ab974dbd37b
Nirvana <37197172+altcnews@users.noreply.github.com>
2018-03-15 21:49:34 +0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -22,53 +22,53 @@ namespace parameters {
 #define CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                  500000000
 #define CRYPTONOTE_MAX_TX_SIZE                          1000000000
 #define CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX         9898 // starts with "VV"
-#define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            10
+#define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            20
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60 * 60 * 2
 
 #define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               15
 
-#define MONEY_SUPPLY                                    1000000000
+#define MONEY_SUPPLY                                    100000000000000000
 #define MIN_MIXIN                                       0
 #define MANDATORY_MIXIN_BLOCK_VERSION                   0
 #define KILL_HEIGHT                                     0
-#define ZAWY_DIFFICULTY_BLOCK_INDEX                     0
+#define ZAWY_DIFFICULTY_BLOCK_INDEX                     30
 #define ZAWY_DIFFICULTY_V2                              1
 #define ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION        0
 
 
 #define EMISSION_SPEED_FACTOR                           18
-#define GENESIS_BLOCK_REWARD                            500000
+#define GENESIS_BLOCK_REWARD                            1000000
 
 #define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 100
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE       100000
-#define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    90000
-#define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1    80000
+#define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    100000
+#define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1    100000
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE
 #define CRYPTONOTE_COINBASE_BLOB_RESERVED_SIZE          600
-#define CRYPTONOTE_MONEY_DECIMAL_POINT                  12
-#define CRYPTONOTE_MONEY_DECIMAL_UNITS                  (uint64_t)pow(10, CRYPTONOTE_MONEY_DECIMAL_POINT)
-#define MINIMUM_FEE                                     (uint64_t)pow(10, CRYPTONOTE_MONEY_DECIMAL_POINT)
-#define DEFAULT_FEE                                     (uint64_t)pow(10, CRYPTONOTE_MONEY_DECIMAL_POINT)
+#define CRYPTONOTE_MONEY_DECIMAL_POINT                  8
+#define CRYPTONOTE_MONEY_DECIMAL_UNITS                  8
+#define MINIMUM_FEE                                     10000
+#define DEFAULT_FEE                                     10000
 #define DEFAULT_DUST_THRESHOLD                          MINIMUM_FEE
 
-#define ALLOY_TRANSACTION_SIZE_LIMIT                  50000
+#define ALLOY_TRANSACTION_SIZE_LIMIT                    50000
 #define MEMPOOL_POLLING_INTERVAL                        60
 
 #define DIFFICULTY_TARGET                               60
 #define EXPECTED_NUMBER_OF_BLOCKS_PER_DAY               24 * 60 * 60 / DIFFICULTY_TARGET
-#define DIFFICULTY_WINDOW                               EXPECTED_NUMBER_OF_BLOCKS_PER_DAY
-#define DIFFICULTY_WINDOW_V1                            DIFFICULTY_WINDOW
-#define DIFFICULTY_WINDOW_V2                            DIFFICULTY_WINDOW
-#define DIFFICULTY_WINDOW_V4                            62
-#define DIFFICULTY_CUT                                  60
-#define DIFFICULTY_CUT_V1                               DIFFICULTY_CUT
-#define DIFFICULTY_CUT_V2                               DIFFICULTY_CUT
+#define DIFFICULTY_WINDOW                               17
+#define DIFFICULTY_WINDOW_V1                            1440
+#define DIFFICULTY_WINDOW_V2                            1440
+#define DIFFICULTY_WINDOW_V4                            1440
+#define DIFFICULTY_CUT                                  0
+#define DIFFICULTY_CUT_V1                               60
+#define DIFFICULTY_CUT_V2                               60
 #define DIFFICULTY_LAG                                  15
-#define DIFFICULTY_LAG_V1                               DIFFICULTY_LAG
-#define DIFFICULTY_LAG_V2                               DIFFICULTY_LAG
+#define DIFFICULTY_LAG_V1                               15
+#define DIFFICULTY_LAG_V2                               15
 
-#define MAX_BLOCK_SIZE_INITIAL                          100 * 1024
-#define MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR           100 * 1024
+#define MAX_BLOCK_SIZE_INITIAL                          100000
+#define MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR           100000
 #define MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR         365 * 24 * 60 * 60 / DIFFICULTY_TARGET
 
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS       1
@@ -82,9 +82,9 @@ namespace parameters {
 #define FUSION_TX_MIN_IN_OUT_COUNT_RATIO                4
 
 #define KEY_IMAGE_CHECKING_BLOCK_INDEX                  0
-#define UPGRADE_HEIGHT_V2                               50000
-#define UPGRADE_HEIGHT_V3                               4294967294
-#define UPGRADE_HEIGHT_V4                               17500
+#define UPGRADE_HEIGHT_V2                               1
+#define UPGRADE_HEIGHT_V3                               30
+#define UPGRADE_HEIGHT_V4                               60
 
 #define UPGRADE_VOTING_THRESHOLD                        90
 #define UPGRADE_VOTING_WINDOW                           EXPECTED_NUMBER_OF_BLOCKS_PER_DAY
@@ -99,7 +99,7 @@ namespace parameters {
 } // parameters
 
 #define CRYPTONOTE_NAME                                 "nirvana"
-#define CRYPTONOTE_GENESIS_TX                           "010a01ff0001808082bf93eff00802ae1818fa177ec6752560ba46b1fe4663830a6de3fd66c3dd5b86077c26c4cab6210176ed284b30c974cde9f363991def48196f6dafaf7db9c858b6fbe5dbd3414bd7"
+#define CRYPTONOTE_GENESIS_TX                           "011401ff000180809aa6eaafe3010207da8cf6d6fe81815d1cfe40fe03b31c1cd5a1caa5466d250973f9f44fd6d2802101ae2b6341c54a757e2b58ae83903e4bed84cd9e313a3a57d4afed047a3f77dd53"
 
 #define TRANSACTION_VERSION_1                           1
 #define TRANSACTION_VERSION_2                           2
@@ -118,8 +118,8 @@ namespace parameters {
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT              4096
 #define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT           1000
 
-#define P2P_DEFAULT_PORT                                12775
-#define RPC_DEFAULT_PORT                                12776
+#define P2P_DEFAULT_PORT                                27125
+#define RPC_DEFAULT_PORT                                27126
 
 #define P2P_LOCAL_WHITE_PEERLIST_LIMIT                  1000
 #define P2P_LOCAL_GRAY_PEERLIST_LIMIT                   5000
@@ -137,8 +137,8 @@ namespace parameters {
 #define P2P_STAT_TRUSTED_PUB_KEY                        "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115"
 
 const std::initializer_list<const char*> SEED_NODES {
-   "45.77.248.163:12775",
-  "104.238.157.86:12775",
+   "45.77.248.163:27125",
+  "104.238.157.86:27125",
 
 };
 
